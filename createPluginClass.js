cfb929664616514f041714b0c5dba37202556ad8
asaf <asf.ktz@gmail.com>
2017-06-25 22:15:56 +0300

    no more dll instance

--- createPluginClass.js
+++ createPluginClass.js
@@ -5,59 +5,58 @@ const { getManifests, getBundles, cacheDir } = require('./paths');
 const webpack = require('webpack');
 const { concat } = require('./utils');
 
-const createPluginClass = dllSettings => {
-  return class Plugin {
-    constructor(options) {
-      this.options = options;
+class Plugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    const { context, inject, entry } = this.options;
+    const dllSettings = { entry };
+
+    getManifests(dllSettings).forEach(manifestPath => {
+      const instance = new webpack.DllReferencePlugin({
+        context: context,
+        manifest: manifestPath
+      });
+
+      instance.apply(compiler);
+    });
+
+    compiler.plugin('before-compile', (params, callback) => {
+      params.compilationDependencies = params.compilationDependencies
+        .filter((path) => !path.startsWith(cacheDir));
+        
+      callback();
+    });
+
+    compiler.plugin('watch-run', (compiler, callback) => {
+      buildIfNeeded(dllSettings, () => createCompiler(dllSettings))
+        .then(log('---- dll created! ---'))
+        .then(() => callback());
+    });
+
+    if (inject) {
+      compiler.plugin('compilation', compilation => {
+        compilation.plugin(
+          'html-webpack-plugin-before-html-generation',
+          (htmlPluginData, callback) => {
+            console.log('injecting scripts to', htmlPluginData.outputName);
+            getBundles(dllSettings).forEach((bundleName) => {
+              console.log('injecting', bundleName);
+            });
+
+            htmlPluginData.assets.js = concat(
+              getBundles(dllSettings),
+              htmlPluginData.assets.js
+            );
+
+            callback();
+          }
+        );
+      });
     }
+  }
+}
 
-    apply(compiler) {
-      const { context, inject } = this.options;
-
-      getManifests(dllSettings).forEach(manifestPath => {
-        const instance = new webpack.DllReferencePlugin({
-          context: context,
-          manifest: manifestPath
-        });
-
-        instance.apply(compiler);
-      });
-
-      compiler.plugin('before-compile', (params, callback) => {
-        params.compilationDependencies = params.compilationDependencies
-          .filter((path) => !path.startsWith(cacheDir));
-          
-        callback();
-      });
-
-      compiler.plugin('watch-run', (compiler, callback) => {
-        buildIfNeeded(dllSettings, () => createCompiler(dllSettings))
-          .then(log('---- dll created! ---'))
-          .then(() => callback());
-      });
-
-      if (inject) {
-        compiler.plugin('compilation', compilation => {
-          compilation.plugin(
-            'html-webpack-plugin-before-html-generation',
-            (htmlPluginData, callback) => {
-              console.log('injecting scripts to', htmlPluginData.outputName);
-              getBundles(dllSettings).forEach((bundleName) => {
-                console.log('injecting', bundleName);
-              });
-
-              htmlPluginData.assets.js = concat(
-                getBundles(dllSettings),
-                htmlPluginData.assets.js
-              );
-
-              callback();
-            }
-          );
-        });
-      }
-    }
-  };
-};
-
-module.exports = createPluginClass;
+module.exports = Plugin;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const createMiddleware = require('./createMiddleware');
 const createPluginClass = require('./createPluginClass');
 
-module.exports = (dllSettings) => ({
-  middleware:   createMiddleware,
-  Plugin:       createPluginClass(dllSettings)
-});
+module.exports = {
+  middleware: createMiddleware,
+  Plugin: createPluginClass
+};
