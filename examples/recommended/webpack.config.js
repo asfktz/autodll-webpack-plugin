531f7a1fecae3b73db9dcff6d9dd68c889605bef
asaf <asf.ktz@gmail.com>
2017-06-30 19:13:01 +0300

    examples

---
+++ examples/recommended/webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+const AutoDllPlugin = require('autodll-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html',
+    }),
+    new AutoDllPlugin({
+      inject: true,
+      context: __dirname,
+      filename: '[name]_[hash].js',
+      path: './dll',
+      entry: {
+        vendor: [
+          'react',
+          'react-dom',
+          'moment'
+        ]
+      }
+    })
+  ]
+};
