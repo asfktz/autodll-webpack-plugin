6983d8099e91baa3e93ea047b85415d90e63b410
asaf <asf.ktz@gmail.com>
2017-07-08 15:08:32 +0300

    accept env param

---
+++ src/createSettings.js
@@ -0,0 +1,30 @@
+import { merge } from './utils';
+import createHash from './createHash';
+import normalizeEntry from './normalizeEntry';
+
+const getInstanceId = (index) => `instance_${index}`;
+const getEnv = (env) => env || process.env.NODE_ENV || 'development';
+
+const createSettings = ({ originalSettings, index }) => {
+  const { entry, env, ...otherSettings } = originalSettings;
+  
+  const defaults = {
+    path: '',
+    entry: null,
+    filename: '[name].js',
+    inject: false,
+    debug: false
+  };
+
+  const settings = merge(defaults, otherSettings, {
+    entry: normalizeEntry(entry),
+    id: getInstanceId(index),
+    env: getEnv(env)
+  });
+
+  return merge(settings, {
+    hash: createHash(settings)
+  });
+};
+
+export default createSettings;
