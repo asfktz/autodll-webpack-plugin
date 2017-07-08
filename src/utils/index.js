2a27d8600c6b27e5121b6a5df940e4a2d324ea40
asaf <asf.ktz@gmail.com>
2017-07-08 15:06:20 +0300

    no need for default export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,3 @@
 export const concat = Array.prototype.concat.bind([]);
-
 export const merge = (...args) => Object.assign({}, ...args);
 export const keys = Object.keys;
-
-export default {
-  concat,
-  merge,
-  keys
-};
