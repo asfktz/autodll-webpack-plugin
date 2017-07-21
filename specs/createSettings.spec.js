789750cfd60a1ecc0ec3932f9401ea698dc18604
asaf <asf.ktz@gmail.com>
2017-07-21 18:43:01 +0300

    Refactor createSettings test

--- specs/createSettings.spec.js
+++ specs/createSettings.spec.js
@@ -1,67 +1,80 @@
 import test from 'ava';
-import createSettings from '../lib/createSettings';
+import mergeAll from 'lodash/fp/mergeAll';
+import { _createSettings } from '../src/createSettings';
+import { _getEnv } from '../src/getEnv';
 
-test('createSettings:', t => {
-  const results = createSettings({
-    index: 9,
-    originalSettings: {
-      entry: {
-        reactStuff: ['react', 'react-dom'],
-        animationStuff: ['pixi.js', 'gsap']
-      }
-    }
-  });
+const fakeProcess = { env: { NODE_ENV: 'FAKE_ENV' } };
+const getEnv = _getEnv(fakeProcess);
+const getContext = () => '/fake_context/';
+const createSettings = _createSettings(getEnv, getContext);
 
-  const expected = {
-    context: process.cwd(),
-    path: '',
+const base = {
+  index: 0,
+  originalSettings: {
     entry: {
       reactStuff: ['react', 'react-dom'],
       animationStuff: ['pixi.js', 'gsap']
-    },
-    filename: '[name].js',
-    inject: false,
-    debug: false,
-    id: 'instance_9',
-    env: 'development',
-    
-    // TODO: test the hash separately.
-    // context is diffrent in every environment,
-    // resulting in different hash for each
-    hash: results.hash
-  };
+    }
+  }
+};
 
-  t.deepEqual(results, expected);
+test('createSettings: basic', t => {
+  const results = createSettings(base);
+  t.snapshot(results);
 });
 
-test('createSettings:', t => {
-  const results = createSettings({
-    index: 9,
-    originalSettings: {
-      debug: true,
-      inject: true,
-      env: 'mars',
-      entry: {
-        reactStuff: ['react', 'react-dom'],
-        animationStuff: ['pixi.js', 'gsap']
+test('createSettings: create hash with env & instance index', t => {
+  {
+    const params = mergeAll([base, { index: 9 }]);
+    const results = createSettings(params);
+    t.is(results.hash, 'FAKE_ENV_instance_9_562e0fe624fe82ce5e6172ccfaa4f900');
+  }
+
+  {
+    const params = mergeAll([
+      base,
+      { index: 2, originalSettings: { env: 'MARS' } }
+    ]);
+
+    const results = createSettings(params);
+
+    t.is(results.hash, 'MARS_instance_2_f25047331adabfbf5310911a578cd763');
+  }
+});
+
+test('createSettings: set the default base options currently', t => {
+  {
+    const results = createSettings(base);
+
+    t.is(results.debug, false);
+    t.is(results.inject, false);
+    t.is(results.filename, '[name].js');
+    t.is(results.path, '');
+    t.is(results.context, '/fake_context/');
+  }
+});
+
+test('createSettings: override the base options currently', t => {
+  {
+    const params = mergeAll([
+      base,
+      {
+        originalSettings: {
+          debug: true,
+          inject: true,
+          filename: '[name].[hash].special.js',
+          path: '/path/to/dll',
+          context: '/override_context/'
+        }
       }
-    }
-  });
+    ]);
 
-  const expected = {
-    context: process.cwd(),
-    path: '',
-    entry: {
-      reactStuff: ['react', 'react-dom'],
-      animationStuff: ['pixi.js', 'gsap']
-    },
-    filename: '[name].js',
-    inject: true,
-    debug: true,
-    id: 'instance_9',
-    env: 'mars',
-    hash: results.hash
-  };
+    const results = createSettings(params);
 
-  t.deepEqual(results, expected);
+    t.is(results.debug, true);
+    t.is(results.inject, true);
+    t.is(results.filename, '[name].[hash].special.js');
+    t.is(results.path, '/path/to/dll');
+    t.is(results.context, '/override_context/');
+  }
 });
