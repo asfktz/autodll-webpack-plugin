8bb7166ce09728b93fe9b0981438b166befa7876
Asaf Katz <asf.ktz@gmail.com>
2017-10-13 23:32:24 +0300

    windows paths

--- src/paths.js
+++ src/paths.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import findCacheDir from 'find-cache-dir';
+const urlJoin = require('url-join');
 
 export const cacheDir = findCacheDir({ name: 'autodll-webpack-plugin' });
 
@@ -7,10 +8,5 @@ export const getManifestPath = hash => bundleName =>
   path.resolve(cacheDir, hash, `${bundleName}.manifest.json`);
 
 export const getInjectPath = ({ publicPath, pluginPath, filename }) => {
-  let injectPublicPath = publicPath;
-  let injectRestPath = path.join(pluginPath, filename);
-  // Ensure that injectPublicPath and injectRestPath can be safely concatinated
-  if (!injectPublicPath.endsWith('/')) { injectPublicPath += '/'; }
-  if (injectRestPath.startsWith('/')) { injectRestPath = injectRestPath.substr(1); }
-  return (injectPublicPath + injectRestPath);
+  return urlJoin(publicPath, pluginPath, filename);
 };
