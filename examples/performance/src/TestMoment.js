d97bdea495475b55b5dc1e23a337a8136cf1bb26
Nargonath <jonas.pauthier@gmail.com>
2017-11-04 11:21:50 +0100

    Add prettier --single-quote. Format code with it. Update lint-staged glob to match project root files

--- examples/performance/src/TestMoment.js
+++ examples/performance/src/TestMoment.js
@@ -1,3 +1,3 @@
 export default function() {
-  return moment().format("LLL");
+  return moment().format('LLL');
 }
--- examples/performance/src/index.js
+++ examples/performance/src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 function render(App) {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(<App />, document.getElementById('root'));
 }
 
 render(App);
 
 if (module.hot) {
-  module.hot.accept("./App", () => {
-    const App = require("./App").default;
+  module.hot.accept('./App', () => {
+    const App = require('./App').default;
     render(App);
   });
 }
