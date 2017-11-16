b8d67a13b71329955f5d874ec0ac3dd7400e0621
Suhas Karanth <sudo.suhas@gmail.com>
2017-11-16 09:02:22 +0530

    Add script for installing fixture deps
    - Refactor fixture folders resolution to `getFixtures.js`.
    - Setup package.json script `install:fixturedeps` to run
      `scripts/installFixtureDeps`.
    - Update travis and appveyor config to `npm run install:fixturedeps`.
    - Run travis build only for master branch and pull requests.

--- scripts/runIntegrationTests.js
+++ scripts/runIntegrationTests.js
@@ -2,34 +2,28 @@
 
 const spawnSync = require('child_process').spawnSync;
 const path = require('path');
-const fs = require('fs');
+const getFixtures = require('./getFixtures');
 
-const cwd = path.join(__dirname, '../specs/fixtures');
-
-const isDirectory = source => fs.lstatSync(path.join(cwd, source)).isDirectory();
-
-const failingTests = fs
-  .readdirSync(cwd)
-  .filter(isDirectory)
-  .filter(dirname => {
-    const result = spawnSync('npm', ['test'], {
-      cwd: path.join(cwd, dirname),
-      shell: true,
-      stdio: 'inherit',
-    });
-
-    if (result.status !== 0) {
-      console.error(`\nIntegration test for "${dirname}" returned status code ${result.status}!`);
-      return true;
-    }
-    return false;
+const failingTests = getFixtures().filter(fixtureDir => {
+  const result = spawnSync('npm', ['test'], {
+    cwd: fixtureDir,
+    shell: true,
+    stdio: 'inherit',
   });
+
+  if (result.status !== 0) {
+    const testname = path.basename(fixtureDir);
+    console.error(`\nIntegration test for "${testname}" returned status code ${result.status}!`);
+    return true;
+  }
+  return false;
+});
 
 if (failingTests.length !== 0) {
   console.error('\nIntegration tests are failing!');
   console.error('The following tests returned non-zero status code:');
   failingTests.forEach(t => {
-    console.error(`  - ${t}`);
+    console.error(`  - ${path.basename(t)}`);
   });
   process.exitCode = 1;
 }
