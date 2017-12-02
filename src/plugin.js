befcd8f7a2622f5e5ff6d9f769c46797588dd93d
Asaf Katz <asf.ktz@gmail.com>
2017-12-02 15:43:45 +0200

    comments

--- src/plugin.js
+++ src/plugin.js
@@ -17,10 +17,29 @@ import createLogger from './createLogger';
 
 class AutoDLLPlugin {
   constructor(settings) {
+    // first, we store a reference to the settings passed by the user as is.
     this._originalSettings = settings;
   }
 
+  // apply is called once when compiler initialized.
+  // note that even if the it called using webpack-dev-server
+  // it still called only once and not on every re-run.
+  // keep in mind that some user wanted to use multiple instances of the plugin in one config,
+  // in that case, each instance calls its own apply.
   apply(compiler) {
+    // createSettings responsibe for extending the defaults values with the user's settings.
+    // It also adds a uniqe hash which in the form of:
+    // [env]_instance_[index]_[settingsHash]
+    // [env] - settings.env provided by the user. defaults to NODE_ENV
+    // [index] - the index of the instance in the user's plugins array.
+    // [settingsHash] - a hash made of JSON.stringify(settings) with some values omitted.
+    // hash example: development_instance_0_3289102229a87e84441ca34609c27500
+
+    // both [env] & [index] aims to solve the challenge of having muliple instances.
+    // in the plugin itself its not a problem,
+    // but since the cache is stored in file system we need to came up with a uniqe path for each instance
+    // to prevent collision. related to: https://github.com/asfktz/autodll-webpack-plugin/issues/30
+
     const settings = createSettings({
       originalSettings: this._originalSettings,
       index: getInstanceIndex(compiler.options.plugins, this),
